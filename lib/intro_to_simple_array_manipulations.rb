eb563eb2575485b09774ee2f574d27bfa6bfef8d
Terranie Sims <terranie67@gmail.com>
2020-04-28 14:11:56 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_concat(my_favorite_things, more_favs) 
+    my_favorite_things = "raindrops on roses", "whiskers on kittens"
+    more_favs = "sports cars", "flatiron school"
+    my_favorite_things.concat(more_favs)
+end
+def using_insert (list_of_programming_languages,another_language)
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  another_language = "Python"
+  list_of_programming_languages.insert(4,another_language)
+end
+def using_uniq(haircuts)
+  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  haircuts.uniq 
+end 
+def using_flatten(instruments)
+  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+  instruments.flatten
+end
+def using_delete(instructors, string)
+  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  string = "Steven"
+  instructors.delete("Steven")
+end 
+def using_delete_at(famous_robots, integer)
+  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+  integer = 2
+  famous_robots.delete_at(2)
+end
